ec4e191c2161fe21aa3366e82e6638a974983fb9
imadobeid86 <imad@spendmatic.com>
2014-12-06 16:11:18 -0500

    android black viewer on capturestop

--- src/android/ScanMatic.java
+++ src/android/ScanMatic.java
@@ -29,6 +29,14 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import com.spendmatic.app.R;
 import android.util.Log;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
 
 public class ScanMatic extends CordovaPlugin {
 
@@ -217,6 +225,10 @@ public class ScanMatic extends CordovaPlugin {
 				try {
 					synchronized(smViewer.smCamera) {
 						smViewer.smCamera.stopPreview();
+						//clear SMViewer image
+						Canvas canvas = smViewer.surfaceHolder.lockCanvas(null);
+		                canvas.drawColor(Color.BLACK);
+		                smViewer.surfaceHolder.unlockCanvasAndPost(canvas);
 					}
 					callbackContext.success();
 				} catch (Exception e) {
